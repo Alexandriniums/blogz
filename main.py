946e6e028c3984dfc5b6e283fc66c5dc7c779dd2
John Doe <ahaderer@gmail.com>
2017-10-14 02:13:47 -0500

    Separated form template into new file, same for newpost and single entry page

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True # setting for 2 things-learning about ORM and how flask apps/apps connect to dbs, and useful for debugging issues when the app isn't talking to your db like you expect it to
+db = SQLAlchemy(app) #calling a sqlalchemy constructor, pass in flask application to bind together. this creates a db object we can now use
 
-all_blog_entries = []
+#data to put in db. create a persistent class (no longer in a list)
+class Blogs(db.Model):
+    
+    #specify datafields that should go into columns w/ this class
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 # these are request handlers below
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
     
+    posts = Blogs.query.all()
+    return render_template('blog_list.html', posts=posts)
+
+
+@app.route('/newpost', methods=['GET', 'POST'])
+def new_post():
+    
     if request.method == 'POST':
-        blog_title = request.form['blog-title']
-        #blog_entry = request.form['blog-enty']
-        all_blog_entries.append(blog_title)
+        title = request.form['blog-title']
+        body = request.form['blog-body']
+        new_post = Blogs(title, body)
+        db.session.add(new_post)
+        db.session.commit()
 
-    return render_template('blog_entries.html', title="Build-a-Blog!", all_blog_entries=all_blog_entries)
+    return render_template('newpost.html')
 
-app.run()
+if __name__ == '__main__':
+    app.run()
+
+#@app.route("/delete-post", methods=['POST'])
+#def delete_entry():
+#    blog_post_id = int(request.form['blog-post-id'])
+#    post = Blogs.query.get(blog_post_id)
+#    db.session.delete(entry)
+#    db.session.commit()
+#
+#    return redirect('/')

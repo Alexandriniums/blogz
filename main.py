35e6fe9ba4746252a84f3fc4c38a9ab0ab4de6e7
John Doe <ahaderer@gmail.com>
2017-10-15 21:28:09 -0500

    Added bonus missions datetime and css stylesheet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -12,10 +13,12 @@ class Blogs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, date):
         self.title = title
         self.body = body
+        self.date = date
 
 @app.route('/')
 def index():
@@ -23,22 +26,25 @@ def index():
     if request.args:
         blog_id = request.args.get('id')
         blog = Blogs.query.get(blog_id)
-        return render_template("blog_entry.html", blog=blog)
+        blog_date = request.args.get('date')
+        return render_template("single_post.html", blog=blog, blog_date=blog_date)
 
     else:
-        posts = Blogs.query.all()
+        #posts = Blogs.query.all()
+        posts = Blogs.query.order_by(Blogs.date.desc()).all()
         return render_template('blog_list.html', title="Build-a-blog!", posts=posts)
 
 
-@app.route('/newpost', methods=['GET', 'POST'])
+@app.route('/blog_form', methods=['GET', 'POST'])
 def new_post():
 
     if request.method == 'GET':
-        return render_template("newpost.html")
+        return render_template("blog_form.html")
 
     if request.method == 'POST':
         title = request.form['blog-title']
         body = request.form['blog-body']
+        date = request.args.get('date')
         title_error = ""
         body_error = ""
 
@@ -49,15 +55,15 @@ def new_post():
             body_error = "Please fill in a blog post."
 
         if not title_error and not body_error:
-            new_post = Blogs(title, body)
+            new_post = Blogs(title, body, date)
             db.session.add(new_post)
             db.session.commit()
             #blog_id = new_post.id
-            # return redirect('/blog_entry?id={0}'.format(blog_id))
+            # return redirect('/single_post?id={0}'.format(blog_id))
             blog_url = "/?id=" + str(new_post.id)
             return redirect(blog_url)
 
-        return render_template("newpost.html", title="Add New Blog Post", title_error=title_error, body_error=body_error)
+        return render_template("blog_form.html", title="Add New Blog Post", title_error=title_error, body_error=body_error)
 
 if __name__ == '__main__':
     app.run()
